f68a4d98123ecb68c6924e0eee9228d82bed2175
Dasbadman <tom-trumper+dasbadman@hotmail.co.uk>
2025-08-19 11:06:02 +0100

    Actually fix up playercreateinfo_action for High Elves

--- dist/IndividualProgressionPatch/zz_highelf_individual_prog.sql
+++ dist/IndividualProgressionPatch/zz_highelf_individual_prog.sql
@@ -6,21 +6,68 @@ UPDATE `playercreateinfo_skills` SET `racemask` = 3255 WHERE `racemask` = 1207 A
 UPDATE `playercreateinfo_skills` SET `racemask` = 3693 WHERE `racemask` = 1645 AND `classMask` = 1 AND `skill` = 54; -- Warrior - Maces
 UPDATE `playercreateinfo_skills` SET `racemask` = 3675 WHERE `racemask` = 1627 AND `classMask` = 1 AND `skill` = 43; -- Warrior - Swords
 
--- Reapply Warrior action bar blatted by individual progression 
-INSERT INTO `playercreateinfo_action` (
+-- Reapply action bars blatted by individual progression 
+-- Human Warrior -> High Elf Warrior
+INSERT IGNORE INTO `playercreateinfo_action` (
    `Race`, `Class`, `Button`, `Action`, `Type`
 )
 SELECT
   12, `Class`, `Button`, `Action`, `Type`
 FROM `playercreateinfo_action`
-WHERE `Race` = 1 AND `Class` = 1
-  AND NOT EXISTS (
-    SELECT 1 FROM `playercreateinfo_action` WHERE `Race` = 12
-  );
--- Correct starting food in action bar for High Elves
-UPDATE `playercreateinfo_action` SET `Action` = 2070 WHERE `Race` = 12 AND `Class` = 2 AND `Button` = 11; -- Paladin - Darnassian Bleu
-UPDATE `playercreateinfo_action` SET `Action` = 117 WHERE `Race` = 12 AND `Class` = 3 AND `Button` = 11;  -- Hunter  - Tough Jerky
-UPDATE `playercreateinfo_action` SET `Action` = 2070 WHERE `Race` = 12 AND `Class` = 4 AND `Button` = 11; -- Rogue   - Darnassian Bleu
-UPDATE `playercreateinfo_action` SET `Action` = 2070 WHERE `Race` = 12 AND `Class` = 5 AND `Button` = 11; -- Priest  - Darnassian Bleu
-UPDATE `playercreateinfo_action` SET `Action` = 2070 WHERE `Race` = 12 AND `Class` = 8 AND `Button` = 11; -- Mage    - Darnassian Bleu
-UPDATE `playercreateinfo_action` SET `Action` = 4604 WHERE `Race` = 12 AND `Class` = 9 AND `Button` = 11; -- Warlock - Forest Mushroom Cap
+WHERE `Race` = 1 AND `Class` = 1;
+-- Human Paladin -> High Elf Paladin
+INSERT IGNORE INTO `playercreateinfo_action` (
+   `Race`, `Class`, `Button`, `Action`, `Type`
+)
+SELECT
+  12, `Class`, `Button`, `Action`, `Type`
+FROM `playercreateinfo_action`
+WHERE `Race` = 1 AND `Class` = 2;
+-- Night Elf Hunter -> High Elf Hunter
+INSERT IGNORE INTO `playercreateinfo_action` (
+   `Race`, `Class`, `Button`, `Action`, `Type`
+)
+SELECT
+  12, `Class`, `Button`, `Action`, `Type`
+FROM `playercreateinfo_action`
+WHERE `Race` = 4 AND `Class` = 3;
+-- Human Rogue -> High Elf Rogue
+INSERT IGNORE INTO `playercreateinfo_action` (
+   `Race`, `Class`, `Button`, `Action`, `Type`
+)
+SELECT
+  12, `Class`, `Button`, `Action`, `Type`
+FROM `playercreateinfo_action`
+WHERE `Race` = 1 AND `Class` = 4;
+-- Human Priest -> High Elf Priest
+INSERT IGNORE INTO `playercreateinfo_action` (
+   `Race`, `Class`, `Button`, `Action`, `Type`
+)
+SELECT
+  12, `Class`, `Button`, `Action`, `Type`
+FROM `playercreateinfo_action`
+WHERE `Race` = 1 AND `Class` = 5;
+-- Human Death Knight -> High Elf Death Knight
+INSERT IGNORE INTO `playercreateinfo_action` (
+   `Race`, `Class`, `Button`, `Action`, `Type`
+)
+SELECT
+  12, `Class`, `Button`, `Action`, `Type`
+FROM `playercreateinfo_action`
+WHERE `Race` = 1 AND `Class` = 6;
+-- Human Mage -> High Elf Mage
+INSERT IGNORE INTO `playercreateinfo_action` (
+   `Race`, `Class`, `Button`, `Action`, `Type`
+)
+SELECT
+  12, `Class`, `Button`, `Action`, `Type`
+FROM `playercreateinfo_action`
+WHERE `Race` = 1 AND `Class` = 8;
+-- Human Warlock -> High Elf Warlock
+INSERT IGNORE INTO `playercreateinfo_action` (
+   `Race`, `Class`, `Button`, `Action`, `Type`
+)
+SELECT
+  12, `Class`, `Button`, `Action`, `Type`
+FROM `playercreateinfo_action`
+WHERE `Race` = 1 AND `Class` = 9;
